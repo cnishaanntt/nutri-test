c83eea3549eb4fe30bdba3a33f5c2d9e6877187f
Nishanth <cnishaanntt@gmail.com>
2018-04-13 08:16:22 +0530

    test-version1

--- www/js/client.js
+++ www/js/client.js
@@ -1,6 +1,6 @@
 console.log('from app');
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('/../../service-worker.js')
+             .register('/../service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
